0549e6466acf2b921b4b52cc1da5e9ffd9bd88f6
João Paulo Pulga <joaopaulo1339@outlook.com>
2019-01-02 17:41:43 -0200

    Basic directory backup works

--- src/config.rs
+++ src/config.rs
@@ -2,6 +2,7 @@
 //! Licensed under the WTFPL License, Version 2.0 <http://www.wtfpl.net/>
 use std::fs::File;
 use std::io::BufReader;
+use std::process::Command;
 
 use serde_derive;
 
@@ -15,6 +16,9 @@ pub struct Config {
 	/// The path to backup
 	path: String,
 
+	/// The destination for all backups files
+	destination: String,
+
 	/// The compress algorithm
 	compress: Option<String>,
 
@@ -25,10 +29,74 @@ pub struct Config {
 	post_backup: Option<Vec<Action>>,
 }
 
-impl Config {
-	/// Check if the path specified in the configuration file is valid
-	pub fn is_valid_path(&self) -> bool {
-		std::path::Path::new(&self.path).exists()
+/// Check if the path specified in the configuration file is valid
+pub fn is_valid_path(cfg: &Config) -> bool {
+	std::path::Path::new(&cfg.path).exists()
+}
+
+/// Run all pre-build task registred for the backup
+pub fn run_pre_backup_tasks(cfg: &Config) {
+	match &cfg.pre_backup {
+		Some(values) => {
+			for v in values {
+				if v.wait {
+					Command::new(&v.command)
+						.spawn()
+						.expect("Error spwaining the process")
+						.wait()
+						.expect("Error in process");
+				} else {
+					Command::new(&v.command)
+						.spawn()
+						.expect("Error spwaining the process");
+				}
+			}
+		}
+		None => (),
+	}
+}
+
+/// Run all post-build task registred for the backup
+pub fn run_post_backup_tasks(cfg: &Config) {
+	match &cfg.post_backup {
+		Some(values) => {
+			for v in values {
+				if v.wait {
+					Command::new(&v.command)
+						.spawn()
+						.expect("Error spwaining the process")
+						.wait()
+						.expect("Error in process");
+				} else {
+					Command::new(&v.command)
+						.spawn()
+						.expect("Error spwaining the process");
+				}
+			}
+		}
+		None => (),
+	}
+}
+
+/// Do the backup and crompress
+pub fn do_backup(cfg: &Config) {
+	let len = match &cfg.compress {
+		Some(v) => v.len(),
+		None => 0,
+	};
+
+	if len == 0 {
+		let f = fs_extra::dir::CopyOptions {
+			overwrite: true,
+			skip_exist: false,
+			buffer_size: 4096,
+			copy_inside: true,
+			depth: 0,
+		};
+		fs_extra::dir::copy(&cfg.path, &cfg.destination, &f).expect(&format!(
+			"Failed to copy folder '{}' to {}",
+			&cfg.path, &cfg.destination,
+		));
 	}
 }
 
--- src/main.rs
+++ src/main.rs
@@ -16,18 +16,24 @@ use clap::Arg;
 
 fn main() {
 	let matches = app_from_crate!()
-		.arg(Arg::with_name("config")
-			.help("Set the configuration file to load")
-			.takes_value(true)
-			.short("c")
-			.long("config")
-			.default_value("backuper.json"))
+		.arg(
+			Arg::with_name("config")
+				.help("Set the configuration file to load")
+				.takes_value(true)
+				.short("c")
+				.long("config")
+				.default_value("backuper.json"),
+		)
 		.get_matches();
 
 	let cfg = config::parse_config(matches.value_of("config").unwrap().to_string()).unwrap();
 	for c in cfg {
-		if c.is_valid_path() {
-			println!("{:#?}", c);
+		if config::is_valid_path(&c) {
+			config::run_pre_backup_tasks(&c);
+			config::do_backup(&c);
+			config::run_post_backup_tasks(&c);
+
+			println!("The task '{}' is sucefull backuped!", c.name)
 		} else {
 			println!("Cannot find the path for the configuration '{}'", c.name)
 		}

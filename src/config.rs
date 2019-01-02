c6438a365ff48546c7d1e06d720ddf0416fabaf2
João Paulo Pulga <joaopaulo1339@outlook.com>
2019-01-02 14:44:38 -0200

    Changed the configuration file and fixed the deserializtion code

--- src/config.rs
+++ src/config.rs
@@ -1,29 +1,62 @@
+#![warn(missing_docs)]
+
+//! Copyright 2019
+//! Licensed under the WTFPL License, Version 2.0 <http://www.wtfpl.net/>
 use std::fs::File;
 use std::io::BufReader;
 
 use serde_derive;
 
-#[derive(Deserialize)]
+/// Configuration struct 
+#[derive(Debug, Deserialize)]
 pub struct Config {
+	/// The configuration name
+	/// Used for error and information messages
 	name: String,
+	
+	/// The path to backup
 	path: String,
+
+	/// The compress algorithm
 	compress: Option<String>,
-	pre_backup: Option<Vec<String>>,
-	post_backup: Option<Vec<String>>
+
+	/// Pre backup actions
+	pre_backup: Option<Vec<Action>>,
+
+	/// Post backup actions
+	post_backup: Option<Vec<Action>>
 }
 
-pub fn parse_config(path : String) -> Result<Config, &'static str> {
-	// Open the file in read-only mode with buffer.
+/// Action struct
+/// 
+/// Actions can run before and after backups
+#[derive(Debug, Deserialize)]
+pub struct Action {
+	/// The command to run in this action
+	command: String,
+
+	/// The backup should wait the action finish before continue with backups? 
+	#[serde(rename = "action")]
+	wait: String
+}
+
+/// Try to parse the specified file to a backuper configuration
+/// 
+/// # Arguments
+/// 
+/// * `path` - The path to for deserialization
+pub fn parse_config(path : String) -> Result<Vec<Config>, String> {
+	// Open the file in read-only mode with buffer
 	let file = match File::open(path) {
 		Ok(f) => f,
-		Err(_) => return Err("Cannot open the configuration file")
+		Err(e) => return Err(format!("Error opening the file: {}", e))
 	};
 
 	let reader = BufReader::new(file);
 
-	let cfg : Config = match serde_json::from_reader(reader) {
+	let cfg : Vec<Config> = match serde_json::from_reader(reader) {
 		Ok(c) => c,
-		Err(_) => return Err("Cannot read the file")
+		Err(e) => return Err(format!("Error parsing the file: {}", e))
 	};
 
 	Ok(cfg)
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
+#![crate_name = "backuper"]
+#![warn(missing_docs)]
+
+//! Copyright 2019
+//! Licensed under the WTFPL License, Version 2.0 <http://www.wtfpl.net/>
+
 #[macro_use]
 extern crate serde_derive;
 
-mod config;
+/// Configuration module
+pub mod config;
 
 fn main() {
-	println!("Hello, world!");
+	let cfg = config::parse_config("backuper.json".to_string()).unwrap();
+	for c in cfg {
+		println!("{:#?}", c);
+	}
 }
